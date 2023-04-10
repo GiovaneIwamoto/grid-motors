a46ff915189ea2c02cdf66d989246e2546cd145c
GiovaneIwamoto <giovaneiwamoto@gmail.com>
2023-04-10 00:51:39 -0400

    docs: swagger

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 import express from 'express';
-
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsDoc from 'swagger-jsdoc';
 import { Router, Request, Response } from 'express';
 import { Connection } from 'mongoose';
 import { CarsRoute, ReservesRoute, UsersRoute } from './routes';
@@ -14,8 +15,26 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//------------------ SERVER ----------------
-app.listen(3000, () => console.log('Server Running on Port 3000... 🤖'));
+//------------------ SWAGGER ----------------
+
+const options = {
+    swaggerOptions: {
+        persistAuthorization: true,
+        showRequestHeaders: false,
+    },
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'GRID MOTORS',
+            version: '1.0.0',
+            description: 'GRID MOTORS API SWAGGER',
+        },
+        servers: [{ url: 'http://localhost:3000/api/v1/' }],
+    },
+    apis: ['./**/*.yaml'],
+};
+const specs = swaggerJsDoc(options);
+app.use('/api/v1/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 //------------------ MONGO DB ----------------
 const DB = (process.env.DATABASE as string).replace(
@@ -30,10 +49,14 @@ mongoose
         useUnifiedTopology: false,
     })
     .then((con: Connection) => {
-        console.log('DB Connection Successful! ☎️');
+        console.log('DB CONNECTION SUCCESSFUL! ⚙️');
     });
 
+//---------------- ROUTES ----------------
 const baseRoute = '/api/v1';
 app.use(baseRoute, CarsRoute);
 app.use(baseRoute, UsersRoute);
 app.use(baseRoute, ReservesRoute);
+
+//------------------ SERVER ----------------
+app.listen(3000, () => console.log('SERVER RUNNING ON PORT 3000 ⚙️'));
